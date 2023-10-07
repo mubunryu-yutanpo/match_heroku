111a0b7fc262bed7e3575ee84afb4f40dda2eb7d
yutaro <1212yutaro@gmail.com>
2023-10-07 12:38:46 +0900

    1007-heroku

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
+
 
 
 class AppServiceProvider extends ServiceProvider
@@ -33,6 +35,7 @@ class AppServiceProvider extends ServiceProvider
 
         // heroku用
         Schema::defaultStringLength(191);
+
 
     }
 }
