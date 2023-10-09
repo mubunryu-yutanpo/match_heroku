b485e1dcd5af1996c875a75205a5e57ebae6ea08
yutaro <1212yutaro@gmail.com>
2023-10-09 10:25:54 +0900

    1009

--- database/seeds/ProjectsTableSeeder.php
+++ database/seeds/ProjectsTableSeeder.php
@@ -16,6 +16,7 @@ class ProjectsTableSeeder extends Seeder
                 'user_id'    => 1,
                 'title'      => 'バナー制作依頼',
                 'type'       => 1,
+                'thumbnail'  => 'uploads/img19.png',
                 'upperPrice' => 30000,
                 'lowerPrice' => 10000,
                 'content'    => 'バナーを作って欲しいんだよね。',
@@ -24,10 +25,21 @@ class ProjectsTableSeeder extends Seeder
                 'user_id'    => 2,
                 'title'      => '【どんな相手でも対応可】魔法少女派遣します',
                 'type'       => 1,
+                'thumbnail'  => 'uploads/img18.png',
                 'upperPrice' => 50000,
                 'lowerPrice' => 4000,
                 'content'    => 'うちの魔法少女は強いので誰が相手でも問題ありません',
             ],
+            [
+                'user_id'    => 3,
+                'title'      => 'こども博士、創造計画',
+                'type'       => 2,
+                'thumbnail'  => 'uploads/image9.png',
+                'upperPrice' => null,
+                'lowerPrice' => null,
+                'content'    => 'こどもの「好き」を伸ばします。こどもが楽しんで自主的に勉強できる環境を作りましょう',
+            ],
+
 
         ];
 
